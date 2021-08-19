364e015ea796ee93d864c3c2be0897a2b6636a30
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2021-08-19 14:03:41 +0300

    Change shell script

--- param_search.sh
+++ param_search.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -t 24:00:00
 #SBATCH --mem=20G
-#SBATCH --array=0-9
+#SBATCH --array=0-35
 #SBTACH --gres=gpu:1 --constraint='pascal|volta'
 #SBATCH -o sp_data/results/param_search_results.out
 
 module load anaconda/2020-05-tf2
 
-python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID
+python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --batch_size 32 --run_name param_search --train_cs_predictor --add_lg_info
