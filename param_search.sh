0f384140930bfe8c96647f31331d732ed2bcbd63
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2021-08-22 16:41:50 +0300

    Some more modif to the shell

--- param_search.sh
+++ param_search.sh
@@ -4,7 +4,8 @@
 #SBATCH --array=0-71
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
+#SBATCH -o slurm_out_folder/p_search.out
 
 module load anaconda
 
-python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --batch_size 32 --run_name param_search_2nd --train_cs_predictor --add_lg_info
+python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --batch_size 32 --run_name param_search_w_nl_nh --train_cs_predictor --add_lg_info
