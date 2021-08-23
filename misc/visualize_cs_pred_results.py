600fe9b7a2d4b02636cacad7622039c983dad231
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2021-08-23 16:30:14 +0300

    Add summary test results for all parameter search models

--- misc/visualize_cs_pred_results.py
+++ misc/visualize_cs_pred_results.py
@@ -12,7 +12,7 @@ def get_cs_acc(life_grp, seqs, true_lbls, pred_lbls, v=False):
             return True
         return False
 
-    def get_acc_for_tolerence(ind, t_lbl, v=False):
+    def get_acc_for_tolerence(ind, t_lbl):
         true_cs = 0
         while t_lbl[true_cs] == "S" and true_cs < len(t_lbl):
             true_cs += 1
@@ -39,23 +39,42 @@ def get_cs_acc(life_grp, seqs, true_lbls, pred_lbls, v=False):
     predictions = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
     predictions = [np.array(p) for p in predictions]
     count, count2 = 0, 0
+    count_tol_fn, count_complete_fn, count_otherSPpred = 0, 0, 0
     for l, s, t, p in zip(life_grp, seqs, true_lbls, pred_lbls):
         life_grp, sp_info = l.split("|")
         ind = 0
         if sp_info == "SP":
             while p[ind] == "S" and ind < len(p) - 1:
                 ind += 1
-            predictions[grp2_ind[life_grp]] += get_acc_for_tolerence(ind, t, v=p)
-            # if get_acc_for_tolerence(ind, t, v=p)[3] == 0:
-            #     count += 1
+            if ind == 0 and life_grp == "POSITIVE" and v and ('L' not in p and 'T' not in p and 'P' not in p and 'M' not in p):
+                count_complete_fn += 1
+                print(p)
+                print(t)
+                print(s)
+                print("\n")
+                count_tol_fn += 1
+            elif ind == 0 and life_grp == "POSITIVE" and v and ('L' in p or 'T' in p  or 'P' in p or 'M' in p):
+                count_otherSPpred += 1
+            elif get_acc_for_tolerence(ind, t)[3] == 0 and v and life_grp == "POSITIVE":
+                count += 1
+                print(p)
+                print(t)
+                print(s)
+                print("\n")
+                count_tol_fn += 1
+            predictions[grp2_ind[life_grp]] += get_acc_for_tolerence(ind, t)
+
+        elif sp_info != "SP" and p[ind] == "S":
+            # count false positive predictions
+            # if v and life_grp=="ARCHAEA":
             #     print(life_grp)
             #     print(p)
             #     print(t)
             #     print(s)
             #     print("\n")
-        elif sp_info != "SP" and p[ind] == "S" and l.split("|")[0] == "EUKARYA":
-            # count false positive predictions
             predictions[grp2_ind[life_grp]] += np.array([0, 0, 0, 0, 0, 1])
+    if v:
+        print(" count_tol_fn, count_complete_fn, count_otherSPpred",  count_tol_fn, count_complete_fn, count_otherSPpred)
     all_recalls = []
     all_precisions = []
     total_positives = []
@@ -170,7 +189,7 @@ def plot_losses(losses, name="param_search_0.2_2048_0.0001_"):
     axs.set_xlabel("Epochs")
     axs.set_ylabel("Loss")
     axs.legend()
-    axs.set_ylim(0,2.5)
+    axs.set_ylim(0,0.2)
     plt.savefig("/home/alex/Desktop/sp6_ds_transformer_nmt_results/" + name+"loss.png")
 
 def plot_mcc(mccs, name="param_search_0.2_2048_0.0001_"):
@@ -235,14 +254,14 @@ def visualize_validation(run="param_search_0.2_2048_0.0001_", folds=[0,1]):
     # extract_and_plot_losses(lines)
 
 
-def extract_results(run="param_search_0.2_2048_0.0001_", folds=[0, 1]):
+def extract_results(run="param_search_0.2_2048_0.0001_", folds=[0, 1], folder='results_param_s_2/'):
     euk_mcc, neg_mcc, pos_mcc, arc_mcc = [], [], [] ,[]
     train_loss, valid_loss = [], []
     cs_recalls_euk, cs_recalls_neg, cs_recalls_pos, cs_recalls_arc = [[], [], [], []], [[], [], [], []], \
                                                                                  [[], [], [], []], [[], [], [], []]
     cs_precs_euk, cs_precs_neg, cs_precs_pos, cs_precs_arc = [[], [], [], []], [[], [], [], []], \
                                                                          [[], [], [], []], [[], [], [], []]
-    with open("results/" + run + "{}_{}.log".format(folds[0], folds[1]), "rt") as f:
+    with open(folder + run + "{}_{}.log".format(folds[0], folds[1]), "rt") as f:
         lines = f.readlines()
     for l in lines:
         if "sp_pred mcc" in l and "VALIDATION" in l:
@@ -305,25 +324,95 @@ def extract_results(run="param_search_0.2_2048_0.0001_", folds=[0, 1]):
     return euk_mcc, neg_mcc, pos_mcc, arc_mcc, train_loss, valid_loss, cs_recalls_euk, cs_recalls_neg, cs_recalls_pos,\
            cs_recalls_arc, cs_precs_euk, cs_precs_neg, cs_precs_pos, cs_precs_arc
 
-def extract_mean_test_results(run="param_search_0.2_2048_0.0001_"):
+def extract_mean_test_results(run="param_search_0.2_2048_0.0001", result_folder="results_param_s_2/"):
     full_dict_results = {}
     epochs = []
     for tr_folds in [[0,1],[1,2],[0,2]]:
-        with open("results/"+run+"{}_{}.log".format(tr_folds[0], tr_folds[1]), "rt") as f:
+        with open(result_folder+run+ "_{}_{}.log".format(tr_folds[0], tr_folds[1]), "rt") as f:
             lines = f.readlines()
             epochs.append(int(lines[-2].split(" ")[2]))
+    avg_epoch = np.mean(epochs)
     print("Results found on epochs: {}, {}, {}".format(*epochs))
 
     for tr_folds in [[0,1],[1,2],[0,2]]:
-        res_dict = pickle.load(open("results/" + run + "{}_{}.bin".format(tr_folds[0], tr_folds[1]), "rb"))
+        res_dict = pickle.load(open(result_folder + run + "_{}_{}_best.bin".format(tr_folds[0], tr_folds[1]), "rb"))
         full_dict_results.update(res_dict)
     life_grp, seqs, true_lbls, pred_lbls = extract_seq_group_for_predicted_aa_lbls(filename="w_lg_w_glbl_lbl_100ep.bin", dict_=full_dict_results)
-    get_pred_accs_sp_vs_nosp(life_grp, seqs, true_lbls, pred_lbls,v=True)
-    get_cs_acc(life_grp, seqs, true_lbls, pred_lbls, v=True)
+    mccs = get_pred_accs_sp_vs_nosp(life_grp, seqs, true_lbls, pred_lbls,v=False)
+    if "param_search_w_nl_nh_0.0_4096_1e-05_4_4" in run:
+        v = True
+    else:
+        v=False
+    all_recalls, all_precisions, _, _, _ = \
+        get_cs_acc(life_grp, seqs, true_lbls, pred_lbls, v=v)
+    return mccs, all_recalls, all_precisions, avg_epoch
+
+def get_best_corresponding_eval_mcc(result_folder="results_param_s_2/", model=""):
+    tr_fold = [[0,1],[1,2],[0,2]]
+    all_best_mccs = []
+    for t_f in tr_fold:
+        with open(result_folder + model + "_{}_{}.log".format(t_f[0], t_f[1])) as f:
+            lines = f.readlines()
+        ep2mcc = {}
+        best_mcc = -1
+        for l in lines:
+            if best_mcc != -1:
+                continue
+            elif "VALIDATION" in l and "mcc" in l:
+                ep, mccs = l.split(":")[2], l.split(":")[4]
+                ep = int(ep.split("epoch")[-1].replace(" ", ""))
+                mccs = mccs.replace(" ", "").split(",")
+                mccs = [float(mcc) for mcc in mccs]
+                ep2mcc[ep] = mccs
+            elif "TEST" in l and "epoch" in l and "mcc" in l:
+                best_ep = int(l.split(":")[2].split("epoch")[-1].replace(" ", ""))
+                best_mcc = ep2mcc[best_ep]
+        all_best_mccs.append(best_mcc)
+    return np.mean(all_best_mccs)
+
+
+
+
+
+
+
+def extract_all_param_results(result_folder="results_param_s_2/"):
+    files = os.listdir(result_folder)
+    unique_params = set()
+    for f in files:
+        if "log" in f:
+            unique_params.add("_".join(f.split("_")[:-2]))
+    mdl2results = {}
+    mdlind2mdlparams = {}
+    # order results by the eukaryote mcc
+    eukaryote_mcc = []
+    for ind, u_p in enumerate(unique_params):
+        mccs, all_recalls, all_precisions, avg_epoch= extract_mean_test_results(run=u_p, result_folder=result_folder)
+        mdl2results[ind] = (mccs, list(np.reshape(np.array(all_recalls),-1)),
+                            list(np.reshape(np.array(all_precisions),-1)), avg_epoch)
+        mdlind2mdlparams[ind] = u_p
+        eukaryote_mcc.append(get_best_corresponding_eval_mcc(result_folder, u_p))
+    best_to_worst_mdls = np.argsort(eukaryote_mcc)[::-1]
+    print("\n\nMCC TABLE\n\n")
+    for mdl_ind in best_to_worst_mdls:
+        print(" & ".join(mdlind2mdlparams[mdl_ind].split("_")[6:]), "&", " & ".join([str(round(mcc,3))
+                     for mcc in mdl2results[mdl_ind][0]]), "&", round(mdl2results[mdl_ind][-1],3), "\\\\ \\hline")
+
+    print("\n\nRecall table \n\n")
+    for mdl_ind in best_to_worst_mdls:
+        print(" & ".join(mdlind2mdlparams[mdl_ind].split("_")[6:]), "&",
+              " & ".join([str(round(rec,3)) for rec in mdl2results[mdl_ind][1]]), "&", round(mdl2results[mdl_ind][-1],3), "\\\\ \\hline")
+
+    print("\n\nPrec table \n\n")
+    for mdl_ind in best_to_worst_mdls:
+        print(" & ".join(mdlind2mdlparams[mdl_ind].split("_")[6:]), "&",
+              " & ".join([str(round(rec,3)) for rec in mdl2results[mdl_ind][2]]), "&", round(mdl2results[mdl_ind][-1],3), "\\\\ \\hline")
+
 
 if __name__ == "__main__":
-    extract_mean_test_results(run="param_search_0_2048_1e-05_")
-    visualize_validation(run="param_search_0_4096_1e-05_", folds=[0,1])
+    # extract_all_param_results()
+    # extract_mean_test_results(run="param_search_0_2048_1e-05")
+    visualize_validation(run="param_search_w_nl_nh_0.0_4096_1e-05_4_4_", folds=[0,1])
     # visualize_validation(run="param_search_0.2_4096_1e-05_", folds=[0,2])
     # visualize_validation(run="param_search_0.2_4096_1e-05_", folds=[1,2])
     # life_grp, seqs, true_lbls, pred_lbls = extract_seq_group_for_predicted_aa_lbls(filename="w_lg_w_glbl_lbl_100ep.bin")
