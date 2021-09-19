a94a57f54815e4d44b1d4d6b21a45ddd6b609c1e
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2021-09-19 20:10:54 +0300

    Small last modification

--- param_search.sh
+++ param_search.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --time=8:00:00
 #SBATCH --mem=35G
-#SBATCH --array=0-14
+#SBATCH --array=0-216
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --constraint='volta'
 
 module load anaconda
 
-python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --dropout 0 --batch_size 32 --run_name beam_search_ --train_cs_predictor --add_lg_info --patience 30 --test_beam
+python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --dropout 0 --batch_size 32 --run_name parameter_search --train_cs_predictor --add_lg_info --patience 30 --test_beam
