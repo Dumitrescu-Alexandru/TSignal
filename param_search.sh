bdcd300adb9bb4168b3fb4299c5a1b4e74cbd210
Alexandru Dumitrescu <dumitrescu.alexandru95@gmail.com>
2022-01-31 18:03:26 +0200

    Load correct module in script

--- param_search.sh
+++ param_search.sh
@@ -6,6 +6,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --constraint='volta'
 
-module load anaconda
+module load anaconda/2020-05-tf2
 
 python main.py --param_set_search_number $SLURM_ARRAY_TASK_ID --batch_size 32 --run_name data_perc_runs --train_cs_predictor --add_lg_info --patience 50 --simplified --very_simplified --validate_on_mcc  --tune_bert --frozen_epochs 15
